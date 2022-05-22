a51546ce9162429654b2fb2afb95da6cf0e1c124
bababooey1234 <timothy.cahill6@education.nsw.gov.au>
2022-05-22 16:41:50 +1000

    Fully working button. Don't use onClick event, there's a massive delay, use OnPointerDown.

--- Assets/NewBehaviourScript.cs
+++ Assets/NewBehaviourScript.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class NewBehaviourScript : MonoBehaviour, IPointerEnterHandler
+public class NewBehaviourScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
 {
     // Start is called before the first frame update
     void Start()
@@ -17,7 +18,17 @@ public class NewBehaviourScript : MonoBehaviour, IPointerEnterHandler
         
     }
 
+    public void OnPointerDown(PointerEventData eventdata){
+        print("Clicked");
+    }
+
     public void OnPointerEnter(PointerEventData eventdata){
         print("Pointer Entered");
+        GetComponent<Image>().color = Color.red;
+    }
+
+    public void OnPointerExit(PointerEventData eventdata){
+        print("Pointer Exited");
+        GetComponent<Image>().color = Color.white;
     }
 }
