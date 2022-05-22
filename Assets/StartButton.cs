d1808836c1070cf9c9be47ae7259e024212ba680
bababooey1234 <timothy.cahill6@education.nsw.gov.au>
2022-05-22 19:20:16 +1000

    Moved hover fading code to inheritable MenuButton class

--- Assets/StartButton.cs
+++ Assets/StartButton.cs
@@ -2,35 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+public class StartButton : MenuButton, IPointerDownHandler
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
+    // Start game when clicked
     public void OnPointerDown(PointerEventData eventdata){
         print("Clicked");
         SceneManager.LoadScene("Main Game");
-    }
-
-    public void OnPointerEnter(PointerEventData eventdata){
-        print("Pointer Entered");
-        StartCoroutine(Fading.FadeTo(Color.grey, .2, GetComponent<Image>()));
-    }
-
-    public void OnPointerExit(PointerEventData eventdata){
-        print("Pointer Exited");
-        StartCoroutine(Fading.FadeTo(Color.white, .2, GetComponent<Image>()));
     }
 }
