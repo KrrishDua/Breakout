60791d86a412675deb6a18da7834ca6e4483dc7b
bababooey1234 <timothy.cahill6@education.nsw.gov.au>
2022-05-24 13:50:16 +1000

    Cool scene switch added. To Do: make button only respond to left click, not right

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -3,12 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartButton : MenuButton, IPointerDownHandler
 {
+    // int screenWidth = 1920, screenHeight = 1080;
+    public static Texture2D screenshot;
     // Start game when clicked
     public void OnPointerDown(PointerEventData eventdata){
         print("Clicked");
-        StartCoroutine(GameObject.Find("Canvas").GetComponent<UIScript>().FadeOutThenSwitch());
+        StartCoroutine(ScreenshotThenSwitch());
+    }
+    // wait until the frame has fully rendered, screenshot it, then switch scenes
+    public IEnumerator ScreenshotThenSwitch(){
+        // wait until frame has fully rendered
+        yield return new WaitForEndOfFrame();
+        // screenshot it
+        // https://docs.unity3d.com/ScriptReference/ScreenCapture.CaptureScreenshotAsTexture.html
+        screenshot = ScreenCapture.CaptureScreenshotAsTexture();
+        // load main game - note that screenshot is not destroyed as it is static
+        SceneManager.LoadScene("Main Game");
     }
 }
--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -9,25 +9,43 @@ public class UIScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //on scene load
-        if(SceneManager.GetActiveScene().name == "Main Game")
-            FadeIn();
+        //on main game load, load in screenshot from title screen, then "zoom out" from it (move and scale the image, not the camera)
+        if(SceneManager.GetActiveScene().name == "Main Game"){
+            GameObject.Find("imgScreenshot").GetComponent<RawImage>().texture = StartButton.screenshot;
+            StartCoroutine(ZoomScreenshot());
+            StartCoroutine(Wait1ThenFade());
+        }
     }
 
-    public void FadeOut(){
-        // fade to black over 1 second
-        StartCoroutine(Fading.FadeTo(Color.black, 1, GameObject.Find("imgFade").GetComponent<Image>()));
+    public void FadeOut(float time){
+        // fade to black over {time} seconds
+        StartCoroutine(Fading.FadeTo(Color.black, time, GameObject.Find("imgFade").GetComponent<Image>()));
     }
 
-    public void FadeIn(){
-        // fade to clear over 1 second
-        StartCoroutine(Fading.FadeTo(Color.clear, 1, GameObject.Find("imgFade").GetComponent<Image>()));
+    public void FadeIn(float time){
+        // fade to clear over {time} seconds
+        StartCoroutine(Fading.FadeTo(Color.clear, time, GameObject.Find("imgFade").GetComponent<Image>()));
     }
-    // slightly hacky. I wanted to fade out the menu screen before switching, but the OnPointerDown handler muse return void, so have to make a different function do it.
-    public IEnumerator FadeOutThenSwitch(){
-        FadeOut();
+    // basically just modified Fading.FadeTo
+    public IEnumerator ZoomScreenshot(){
+        // took a long time to find the right RectTransform properties - anchoredPosition is position relative to anchor at centre of UI, sizeDelta is effectively just size
+        RectTransform rt = GameObject.Find("imgScreenshot").GetComponent<RectTransform>();
+        Vector3 startPos = rt.anchoredPosition, targetPos = new Vector3(-365, 149, 0), differencePos = targetPos - startPos;
+        Vector2 startSize = rt.sizeDelta, targetSize = new Vector2(81, 45), differenceSize = targetSize - startSize;
+        // same as Fading.FadeTo
+        float proportion;
+        for(float timePassed=0; (proportion=timePassed/2)<=1; timePassed+=Time.deltaTime){
+            rt.anchoredPosition = startPos + Vector3.Scale(differencePos, new Vector3(proportion, proportion, 1));
+            rt.sizeDelta = startSize + Vector2.Scale(differenceSize, new Vector2(proportion, proportion)); //vectors use Scale not * like Color
+            yield return null;
+        }
+        //set to exactly right position at the end
+        rt.anchoredPosition = targetPos;
+        rt.sizeDelta = targetSize;
+    }
+
+    public IEnumerator Wait1ThenFade(){
         yield return new WaitForSeconds(1);
-        SceneManager.LoadScene("Main Game");
-        CursorHelper.CursorNormal();
+        FadeIn(1);
     }
 }
