00d0e8b1b583413994b9c3193132265cbb09e9b2
bababooey1234 <timothy.cahill6@education.nsw.gov.au>
2022-05-27 13:03:55 +1000

    made x button work. currently no way to start game

---
+++ Assets/Scripts/ErrorButton.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ErrorButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler
+{
+    [HideInInspector] public static bool errorVisible;
+    public void OnPointerEnter(PointerEventData eventdata){
+        if(errorVisible){
+            StartCoroutine(Fading.FadeTo(Color.white, .1f, GetComponent<Image>()));
+            CursorHelper.CursorHand();
+        }
+    }
+    public void OnPointerExit(PointerEventData eventdata){
+        StartCoroutine(Fading.FadeTo(new Color(1,1,1,0), .1f, GetComponent<Image>()));
+        CursorHelper.CursorNormal();
+    }
+    public void OnPointerDown(PointerEventData eventdata){
+        print("clicked");
+        StartCoroutine(Fading.FadeTo(new Color(1,1,1,0), .2f, GameObject.Find("imgFade").GetComponent<Image>())); //half-fade to white
+        GameObject.Find("imgError").GetComponent<Image>().color = Color.clear; // show error message
+        ErrorButton.errorVisible = false;
+        GetComponent<Image>().color = Color.clear;
+        CursorHelper.CursorNormal();
+    }
+    public void ErrorSequence(){
+        StartCoroutine(Fading.FadeTo(new Color(1,1,1,.3f), .2f, GameObject.Find("imgFade").GetComponent<Image>())); //half-fade to white
+        GameObject.Find("imgError").GetComponent<Image>().color = Color.white; // show error message
+        ErrorButton.errorVisible = true;
+        GameObject.Find("sndError").GetComponent<AudioSource>().Play(0); //play error sound
+    }
+}
--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -7,8 +7,7 @@ using UnityEngine.UI;
 
 public class StartButton : MenuButton, IPointerDownHandler
 {
-    public static Texture2D screenshotEmpty;
-    public static Texture2D screenshotError;
+    public static Texture2D screenshot;
     //make sndError not unload after scene change
     public void Start(){
         DontDestroyOnLoad(GameObject.Find("sndError"));
@@ -19,23 +18,21 @@ public class StartButton : MenuButton, IPointerDownHandler
         // if button is not left click, return
         if(eventdata.button != PointerEventData.InputButton.Left)return;
         print("Clicked");
-        StartCoroutine(ErrorSequence());
+        //StartCoroutine(ErrorSequence());
+        GameObject.Find("imgErrorHover").GetComponent<ErrorButton>().ErrorSequence();
     }
     // wait until the frame has fully rendered, screenshot it, TODO finish this comment
     public IEnumerator ErrorSequence(){
         interactable = false;
         GameObject.Find("btnStart").GetComponent<Image>().color = Color.white;
         yield return new WaitForEndOfFrame();
-        screenshotEmpty = ScreenCapture.CaptureScreenshotAsTexture();
-        StartCoroutine(Fading.FadeTo(new Color(1,1,1,.3f), .2f, GameObject.Find("imgFade").GetComponent<Image>())); //half-fade to white
-        GameObject.Find("imgError").GetComponent<Image>().color = Color.white; // show error message
-        GameObject.Find("sndError").GetComponent<AudioSource>().Play(0); //play error sound
+        screenshot = ScreenCapture.CaptureScreenshotAsTexture();
         yield return new WaitForSeconds(2); // wait one second
         yield return new WaitForEndOfFrame();// wait until frame has fully rendered
         // screenshot it
         // https://docs.unity3d.com/ScriptReference/ScreenCapture.CaptureScreenshotAsTexture.html
-        screenshotError = ScreenCapture.CaptureScreenshotAsTexture();
+        //screenshotError = ScreenCapture.CaptureScreenshotAsTexture();
         // load main game - note that screenshot is not destroyed as it is static
-        SceneManager.LoadScene("Main Game");
+        //SceneManager.LoadScene("Main Game");
     }
 }
