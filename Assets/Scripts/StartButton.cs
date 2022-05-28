2def545fa6c730066dd0f287e1464fb585c51221
KrrishDua <105398646+KrrishDua@users.noreply.github.com>
2022-05-29 01:08:29 +1000

    Added seamless background music between scene switch + fixed sound effect

--- Assets/Scripts/StartButton.cs
+++ Assets/Scripts/StartButton.cs
@@ -18,7 +18,7 @@ public class StartButton : MenuButton, IPointerDownHandler
         // if button is not left click, return
         if(eventdata.button != PointerEventData.InputButton.Left)return;
         print("Clicked");
-        //StartCoroutine(ErrorSequence());
+        StartCoroutine(ErrorSequence());
         GameObject.Find("imgErrorHover").GetComponent<ErrorButton>().ErrorSequence();
     }
     // wait until the frame has fully rendered, screenshot it, TODO finish this comment
@@ -27,12 +27,13 @@ public class StartButton : MenuButton, IPointerDownHandler
         GameObject.Find("btnStart").GetComponent<Image>().color = Color.white;
         yield return new WaitForEndOfFrame();
         screenshot = ScreenCapture.CaptureScreenshotAsTexture();
-        yield return new WaitForSeconds(2); // wait one second
-        yield return new WaitForEndOfFrame();// wait until frame has fully rendered
+        //yield return new WaitForSeconds(2); // wait one second
+        //yield return new WaitForEndOfFrame();// wait until frame has fully rendered
         // screenshot it
         // https://docs.unity3d.com/ScriptReference/ScreenCapture.CaptureScreenshotAsTexture.html
         //screenshotError = ScreenCapture.CaptureScreenshotAsTexture();
         // load main game - note that screenshot is not destroyed as it is static
-        //SceneManager.LoadScene("Main Game");
+        SceneManager.LoadScene("Main Game");
+        SfxManager.sfxInstance.Audio.PlayOneShot(SfxManager.sfxInstance.Click);
     }
 }
